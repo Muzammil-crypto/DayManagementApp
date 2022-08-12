160712af5b6e60c1d5756e6510e3162588f04c86
Muzammil Rafiq <57823963+Muzammil-crypto@users.noreply.github.com>
2022-08-12 13:20:21 +0500

    deletion added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import TodoList from "./MyComponents/TodoList";
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+
   return (
     <div>
       <header>
-        <h1>Muzammil's Todo List </h1>
+        <h1>Schedule Your Day Here!</h1>
       </header>
       <Form
         inputText={inputText}
         todos={todos}
         setTodos={setTodos}
         setInputText={setInputText}
+        setStatus={setStatus}
       />
       <TodoList setTodos={setTodos} todos={todos} />
     </div>
